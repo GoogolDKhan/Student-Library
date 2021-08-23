904cc7667cb2fd9235ad3ed9aaf28589a301540f
Sarfaraz <khansarfaraz2141@gmail.com>
2021-08-23 10:05:25 +0530

    Format code using black

--- main.py
+++ main.py
@@ -14,18 +14,22 @@ class Library:
     def borrow_book(self, book_name):
         if book_name in self.books:
             print(
-                f"You have been issued {book_name}. Please keep it safe and return it within 30 days")
+                f"You have been issued {book_name}. Please keep it safe and return it within 30 days"
+            )
             self.books.remove(book_name)
             return True
         else:
             print(
-                "Sorry. This book is either not available or has already been issued to someone else.")
+                "Sorry. This book is either not available or has already been issued to someone else."
+            )
             return False
 
     # Method to return a book from the student to library
     def return_book(self, book_name):
         self.books.append(book_name)
-        print("Thanks for returning this book! Hope you enjoyed reading this book. Have a great day ahead!")
+        print(
+            "Thanks for returning this book! Hope you enjoyed reading this book. Have a great day ahead!"
+        )
 
 
 class Student:
@@ -44,19 +48,27 @@ class Student:
 if __name__ == "__main__":
 
     # Book available in thw Central Library
-    central_library = Library(['Material Science', 'Mechanics of Solids', 'Fluid Mechanics',
-                             'Dynamics of Machines', 'Manufacturing Process', 'Heat Transfer'])
+    central_library = Library(
+        [
+            "Material Science",
+            "Mechanics of Solids",
+            "Fluid Mechanics",
+            "Dynamics of Machines",
+            "Manufacturing Process",
+            "Heat Transfer",
+        ]
+    )
 
     student = Student()
-    while(True):
-        welcome_message = '''
+    while True:
+        welcome_message = """
         =====Welcome to Central Library=====
         Please choose an option:
         1. List all the books
         2. Request a book
         3. Add or Return a book
         4. Exit the library
-        '''
+        """
         print(welcome_message)
         choice = int(input("Enter a choice: "))
         if choice == 1:
